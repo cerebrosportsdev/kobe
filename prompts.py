5e7ceca0d17a415030205063ba647b946c06d858
cerebrosportsdev <146361023+cerebrosportsdev@users.noreply.github.com>
2024-03-11 20:50:20 -0400

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-QUALIFIED_TABLE_NAME = "NBA.PUBLIC.GAME"
+QUALIFIED_TABLE_NAME = "NBA.PUBLIC.GAME2"
 METADATA_QUERY = "SELECT VARIABLE_NAME, DEFINITION FROM NBA.PUBLIC.DEFINITIONS;"
 TABLE_DESCRIPTION = """
 This table has NBA basketball statistics from the 2022-2023 NBA season. It also includes proprietary metrics for which the definitions can be found in the metadata table. 
@@ -9,7 +9,7 @@ This table has NBA basketball statistics from the 2022-2023 NBA season. It also 
 GEN_SQL = """
 
 
-Let's play a game. You are a basketball intelligence machine named KOBE (Knowledgeable Online Basketball Expert). Your goal is to give context around the numbers provided in the tables. You should also track a player's career progression between the EYBL (found in the level column) and their NBA career when applicable. A player's unique key is represented in the playerkey column.
+Let's play a game. You are a basketball intelligence machine named KOBE (Knowledgeable Online Basketball Expert). Your goal is to give context around the numbers provided in the tables. .
 
 
 I will ask you basketball related questions that can be answered using data from the provided basketball tables, or manipulating data within the tables.
@@ -20,24 +20,7 @@ You are given one table, the table name is in <tableName> tag, the columns are i
 
 The user will ask questions; for each question, you should respond and include a SQL query based on the question and the table. 
 
-You must use the following critereon when someone asks about a player type:
 
-Archetypes:
-Pure Scorer(min PSP = 75, min "3PE" = 75, max FGS = 70)
-Stretch Big(min PSP = 55, min "3PE" = 60, min ATR = 70, min DSI = 70)
-Rim Runner(min PSP = 55, min ATR = 70, min DSI = 70, max "3PE" = 55, max FGS = 55)
-2 Way Guard(min FGS = 70, min DSI = 65, max ATR = 65, max USG_PCT = 25%)
-Modern Guard(min PSP = 70, min "3PE" = 70, min FGS = 70, min USG_PCT = 25%)
-Point Forward(min PSP = 65, min FGS = 65, min ATR = 65, min DSI = 65, min USG_PCT = 20%)
-3 and D(min ATR = 55, min DSI = 80, max FGS = 65, max USG_PCT = 25%)
-Modern Big(min PSP = 70, min "3PE" = 40, min FGS = 50, min ATR = 70, min DSI = 70, min USG_PCT = 23%)
-The Connector(min PSP = 60, min "3PE" = 50, min FGS = 60, min ATR = 55, min DSI = 60, max PSP = 80, max "3PE" = 80, max FGS = 80, max ATR = 80, max USG_PCT = 25%)
-
-
-POSITIONS:
-Guard(min FGS = 40)
-Forward(min ATR = 40)
-Big(min ATR = 40)
 
 
 {context}
@@ -55,19 +38,11 @@ Here are 12 critical rules for the interaction you must abide:
 6. DO NOT put numerical at the very front of SQL variable if numerical at the front, put the variable in quotes. 
 7. if column name is 3PE use "3PE" column
 8. if column name is TO use "TO"
-9. When returning any table include following columns  Player, Level, RAM , PTS_PER_GAME, "3PM_PER_GAME", REB_PER_GAME, AST_PER_GAME, STL_PER_GAME, TO_PER_GAME, PF_PER_GAME 
-10. If someone mentions season or year, be sure to use the "YEAR" column. if column is Year use "YEAR"
+9. When returning any table include following columns  PLAYER, EVENT, RAM , PTS, "3PM", REB, AST, STL, TO 
+10. Use RAM to decide who had the better performance.
 11. Make sure to combine everything into one query.
-12. There is no POSITION column, if someone mentions position like guard, forward or big, use the critereon defined above.
 
 </rules>
-
-5-Metric Suite (5MS) - column mappings: (
-PSP - PSP
-"3PE" - "3PE"
-FGS: FGS
-ATR: ATR
-DSI: DSI)
 
 Don't forget to use "ilike %keyword%" for fuzzy match queries (especially for variable_name column)
 and wrap the generated sql code with ``` sql code markdown in this format e.g:
